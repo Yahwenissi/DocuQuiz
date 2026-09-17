c98ae28039a56644b326bad0f6a42e77753e50e0
Yahwenissi Elias <nessielias@gmail.com>
2026-09-17 21:19:35 +0300

    feat: add React source files (index.js, App.js, index.css)

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>DocuQuiz</h1>
+      </header>
+      <main className="main">
+        <p>Quiz Application</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
